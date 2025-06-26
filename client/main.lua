73e963eb8734f67d8a0fd704c71dcebab559fc87
Reyghita Hafizh <reyhafizh07@gmail.com>
2025-06-26 21:18:38 +0700

    Add one-time petrol can weapon registration
    Introduces a REGISTER flag to ensure the petrol can weapon is given and removed from the player only once when a weapon is cached. This may be used for initialization or registration purposes.

--- client/main.lua
+++ client/main.lua
@@ -1,9 +1,17 @@
 local config = require 'config.client'
 
+local REGISTER = false
+
 lib.onCache('weapon', function (weapon)
     if not weapon then
         ResetPedWeaponMovementClipset(cache.ped)
         return
+    end
+
+    if not REGISTER then
+        GiveWeaponToPed(cache.ped, joaat("weapon_petrolcan"), 0, false, true)
+        RemoveWeaponFromPed(cache.ped, joaat("weapon_petrolcan"))
+        REGISTER = true
     end
 
     local clipSet = config[weapon]
